9cafcb396c22d65408de65d1a56d70b5a960dab5
Albert Fougy <albert@fougy.com>
2018-06-27 09:12:36 -0400

    Revert "going back to original working file"
    This reverts commit fb56b1413126bbb59647502f1867f0d8d8af632d.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 'use strict';
-/* global  api store  bookmarkList */
+/* global  api store  bookmarkList Item */
+
+
 
 
 $(document).ready(function() {
@@ -13,3 +15,28 @@ $(document).ready(function() {
   bookmarkList.bindEventListeners();
 });
 
+
+
+
+// bookmarkList.handleDeleteItemClicked();
+// bookmarkList.render();
+
+
+// $('#bookmark-add-btn').on("click", event => {
+//   const title = $('#js-add-bookmark input[name="bookmark-title"]')[0].value;
+//   const url = $('#js-add-bookmark input[name="bookmark-url"]')[0].value;
+//   const desc = $('#js-add-bookmark textarea[name="bookmark-desc"]')[0].value;
+//   const rating = parseInt($('#bookmark-rating')[0].value);
+
+
+//   // ToDo Validation
+//   // title ( use simple string methods to validate)
+//   // url(must start with http(s): Use Regex
+
+//   api.createItem({title, url, desc, rating}, response => {
+//     console.log('Bookmark created successfully', response)
+//     bookmarkList.render();
+//   });
+
+
+// });
